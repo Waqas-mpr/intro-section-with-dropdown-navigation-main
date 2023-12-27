9d15dd2ae3bdc271c9e5138f3e85d74558313538
Waqas Ahmed <Waqas-mpr@users.noreply.github.com>
2023-12-27 22:22:58 +0500

    add menu

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const toggleUi = document.querySelectorAll(".toggle-ui");
-const dropdown = document.querySelectorAll(".dropdown");
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
 
-const clicked = -1;
-dropdown.forEach((nav) => {
-	nav.addEventListener("click", (e) => {
-		console.log(nav.children.item(1).children);
-		nav.children[0].children[0].classList.toggle("toggle-ui");
-		nav.children[0].children[1].classList.toggle("toggle-ui");
-		nav.children[1].classList.toggle("toggle-ui");
-	});
-});
+const items = document.querySelectorAll(".item");
+
+const iconOpen = document.querySelector(".icon-open-menu");
+const iconClose = document.querySelector(".icon-close-menu");
+
+function toggleMenu() {
+	if (menu.classList.contains("active")) {
+		menu.classList.remove("active");
+		iconOpen.classList.remove("d-none");
+		iconClose.classList.add("d-none");
+	} else {
+		menu.classList.add("active");
+		iconOpen.classList.add("d-none");
+		iconClose.classList.remove("d-none");
+	}
+}
+
+toggle.addEventListener("click", toggleMenu, false);

dc1c8ebeafa0ff491c4bb91b8dc75c096b1c7cdb
Waqas Ahmed <Waqas-mpr@users.noreply.github.com>
2023-12-28 16:56:12 +0500

    mobile version completed

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 
-const items = document.querySelectorAll(".item");
-
 const iconOpen = document.querySelector(".icon-open-menu");
 const iconClose = document.querySelector(".icon-close-menu");
 
@@ -19,3 +17,35 @@ function toggleMenu() {
 }
 
 toggle.addEventListener("click", toggleMenu, false);
+
+/* Activate Submenu */
+const items = document.querySelectorAll(".item");
+
+function toggleItem() {
+	if (this.classList.contains("submenu-active")) {
+		this.classList.remove("submenu-active");
+	} else if (menu.querySelector(".submenu-active")) {
+		menu.querySelector(".submenu-active").classList.remove("submenu-active");
+		this.classList.add("submenu-active");
+	} else {
+		this.classList.add("submenu-active");
+	}
+}
+
+for (let item of items) {
+	if (item.querySelector(".submenu")) {
+		item.addEventListener("click", toggleItem, false);
+	}
+	item.addEventListener("keypress", toggleItem, false);
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+	let isClickInside = menu.contains(e.target);
+
+	if (!isClickInside && menu.querySelector(".submenu-active")) {
+		menu.querySelector(".submenu-active").classList.remove("submenu-active");
+	}
+}
+
+document.addEventListener("click", closeSubmenu, false);
